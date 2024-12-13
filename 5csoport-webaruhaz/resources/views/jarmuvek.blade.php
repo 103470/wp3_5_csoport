0075bf7d0f3d9864ecb1265fb59f8e7693abd97a
halfalo <jj3nz0@std.uni-eszterhazy.hu>
2024-12-13 13:08:53 +0100

    Adatbázis feltöltése
    feltöltés autókkal, képek, adatok megjelenítése

--- 5csoport-webaruhaz/resources/views/jarmuvek.blade.php
+++ 5csoport-webaruhaz/resources/views/jarmuvek.blade.php
@@ -1,1 +1,33 @@
 @extends('index')
+@section('title', 'Webshop')
+@section('content')
+    <main class="container">
+        <section>
+            <div class="row">
+                @foreach ($jarmuvek as $jarmu)
+                <div class="col-lg-6">
+                    <center>
+                        <img src={{$jarmu->kep}} style="height:150px;width:300px;" alt="">
+
+                    <div>
+                        <p>
+                            {{$jarmu->id}}
+                            {{$jarmu->gyarto}}
+                            {{$jarmu->tipus}}
+                            {{$jarmu->motor}}
+                            {{$jarmu->uzemanyag}}
+                            {{$jarmu->hajtas}}
+                            {{$jarmu->karosszeria}}
+                            {{$jarmu->ajtokSzama}}
+                            {{$jarmu->ar}}
+                        </p> 
+                    </div>
+                    </center>
+                    
+                </div>
+                
+                @endforeach
+            </div>
+        </section>
+    </main>
+@endsection
--- 5csoport-webaruhaz/routes/web.php
+++ 5csoport-webaruhaz/routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\JarmuvekManager;
 use App\Http\Controllers\ProductController;
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Container\Attributes\DB;
 use Illuminate\Support\Facades\DB as FacadesDB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ProductController::class, 'index']);
+Route::get('/', [JarmuvekManager::class, "index"]) -> name("home");
 
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'store'])->name('register.store');
@@ -16,5 +17,6 @@ Route::get('/login', [AuthController::class, "login"])->name('login');
 Route::post('/login', [AuthController::class, "authenticate"])->name('authenticate');
 
 Route::get('/logout', [AuthController::class, "logout"])->name('logout');
+Route::post('/logout', [AuthController::class, "authenticate"])->name('authenticate');
 
 Route::get('/db-test', function () { try { FacadesDB::connection()->getPdo(); return 'Sikeres adatbázis kapcsolat!'; } catch (\Exception $e) { return 'Adatbázis kapcsolat hiba: ' . $e->getMessage(); } });
