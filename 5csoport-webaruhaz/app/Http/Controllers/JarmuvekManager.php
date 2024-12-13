8b6b3fad39b36213b91a31316d6638a8f0171748
halfalo <jj3nz0@std.uni-eszterhazy.hu>
2024-12-13 10:24:00 +0100

    Jarmuvek controller létrehozása

--- 5csoport-webaruhaz/app/Http/Controllers/JarmuvekManager.php
+++ 5csoport-webaruhaz/app/Http/Controllers/JarmuvekManager.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jarmuvek;
 use Illuminate\Http\Request;
 
 class JarmuvekManager extends Controller
 {
-    //
+    function index()
+    {
+        $jarmuvek = Jarmuvek::all();
+        return view('jarmuvek', compact('jarmuvek'));
+    }
 }
